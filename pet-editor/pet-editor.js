37c33d76e0f9d59c9ac52c56d1602a6ec171da2d
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-10-03 13:00:42 -0700

    Add a pet

--- pet-editor/pet-editor.js
+++ pet-editor/pet-editor.js
@@ -4,6 +4,7 @@ import '../auth/user.js';
 // > Part A: import upload image
 import { uploadImage } from '../fetch-utils.js';
 // > Part B: import fetch to create a pet
+import { createPet } from '../fetch-utils.js';
 
 /* Get DOM Elements */
 const petForm = document.getElementById('pet-form');
@@ -38,13 +39,23 @@ petForm.addEventListener('submit', async (e) => {
 
     const pet = {
         // > Part B: add the name, bio, and image_url fields to the pet object
+        name: formData.get('name'),
+        bio: formData.get('bio'),
+        image_url: response,
     };
 
     // > Part B:
     //    - call function to create the pet in the database
+    const reply = await createPet(pet);
     //    - store the error and pets state from the response
+    error = response.error;
     //    - either display the error or redirect the user to the home page
 });
+if (error) {
+    displayError();
+} else {
+    // location.assign('/');
+}
 
 /* Display Functions */
 
